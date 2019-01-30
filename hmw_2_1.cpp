31130d953fa0471bcab11b3d6e096d1d0e22909a
trimpage <45026144+trimpage@users.noreply.github.com>
2019-01-29 19:49:09 -0800

    Update hmw_2_1.cpp

--- hmw_2_1.cpp
+++ hmw_2_1.cpp
@@ -7,37 +7,44 @@ Arian Owji
 */
 
 #include <iostream>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
 int main()
 {
-	int max = -2147483647;
-	int min = 2147483647;
-	int x;
-	char response = 'y';
+    int max = -2147483647;
+    int min = 2147483647;
+    int x;
+    char response = 'y';
+    
+    while (response == 'y') {
+        std::cout << "Enter a list of integers: ";
 
-	while (response == 'y') {
-		std::cout << "Enter a list of integers: ";
-
-		while (cin >> x) {
-			if (x > max) {
-				max = x;
-			}
-			if (x < min) {
-				min = x;
-			}
-		}
-
-		std::cout << "minimum integer in the list is " << min << endl;
-		std::cout << "maximum integer in the list is " << max << endl << endl;
-
-		std::cout << "Continue? <y/n> ";
-		std::cin.clear();
-		std::cin.ignore(10000, '\n');
-		std::cin >> response;
-		std::cout << endl;
-	}
-
-	return 0;
+        string line;
+        getline(std::cin, line);
+        stringstream ss(line);
+        
+        while (!ss.eof()) {
+            ss >> x;
+            
+            if (x > max) {
+                max = x;
+            }
+            if (x < min) {
+                min = x;
+            }
+        }
+        
+        std::cout << "minimum integer in the list is " << min << endl;
+        std::cout << "maximum integer in the list is " << max << endl << endl;
+        
+        std::cout << "Continue? <y/n> ";
+        std::cin >> response;
+        std::cout << endl;
+        std::cin.get();
+    }
+    
+    return 0;
 }
