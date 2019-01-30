05cb295ce40412a17b2b0c9fa4fdd0a0ed0aaec2
trimpage <45026144+trimpage@users.noreply.github.com>
2019-01-29 19:55:58 -0800

    Update hmw_2_3.cpp

--- hmw_2_3.cpp
+++ hmw_2_3.cpp
@@ -7,44 +7,51 @@ Arian Owji
 */
 
 #include <iostream>
+#include <string>
+#include <sstream>
 #include <cmath>
 
 using namespace std;
 
 int main()
 {
-	char response = 'y';
-	double x;
-	int count = 0;
-	double sum = 0;
-	double sumSquares = 0; 
-
-	while (response == 'y') {
-		std::cout << "Enter a list of values <of type double>: ";
-
-		   while (std::cin >> x) {
-			count++;
-			sum += x;
-			sumSquares += (pow(x, 2));
-		}
-		
-		double mean = sum / count;
-		double deviation = (sqrt((sumSquares - ((pow(sum, 2)) / count)) / (count - 1)));
-
-		std::cout << "Number = " << count << endl;
-		std::cout << "Mean = " << mean << endl;
-		std::cout << "Deviation = " << deviation << endl;
-	
-		std::cout << endl;
-		std::cin.clear();
-		std::cin.ignore(10000, '\n');
-		std::cout << "Continue? <y/n> ";
-		std::cin >> response;
-		std::cout << endl;
-		
-		count = 0;
-		sum = 0;
-		sumSquares = 0;
-	}
-	return 0;
+    char response = 'y';
+    double x;
+    int count = 0;
+    double sum = 0;
+    double sumSquares = 0;
+    
+    while (response == 'y') {
+        std::cout << "Enter a list of values <of type double>: ";
+        
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        
+        while (!ss.eof()) {
+            ss >> x;
+            
+            count++;
+            sum += x;
+            sumSquares += (pow(x, 2));
+        }
+        
+        double mean = sum / count;
+        double deviation = (sqrt((sumSquares - ((pow(sum, 2)) / count)) / (count - 1)));
+        
+        std::cout << "Number = " << count << endl;
+        std::cout << "Mean = " << mean << endl;
+        std::cout << "Deviation = " << deviation << endl;
+        
+        std::cout << endl;
+        std::cout << "Continue? <y/n> ";
+        std::cin >> response;
+        std::cout << endl;
+        std::cin.get();
+        
+        count = 0;
+        sum = 0;
+        sumSquares = 0;
+    }
+    return 0;
 }
