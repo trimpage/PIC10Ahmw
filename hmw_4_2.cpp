c1e1c8c6aaad9b62a3b2ce5c3a2c1605ed0c1b19
trimpage <45026144+trimpage@users.noreply.github.com>
2019-02-13 21:06:37 -0800

    Update hmw_4_2.cpp

--- hmw_4_2.cpp
+++ hmw_4_2.cpp
@@ -17,27 +17,53 @@ struct calc_bin_data {
 	char op;
 };
 
-calc_bin_data a;
-calc_bin_data b;
-calc_bin_data c;
-
 calc_bin_data calc_parsing(const string& s) {
+	calc_bin_data a;
+
 	unsigned first = s.find("<");
 	unsigned last = s.find(">");
-	
-	a.x1 = s.substr(first + 1, last - first - 1);
+
+	a.x1 = s.substr(first + 1, last - 1);
 
 	string s2 = s.substr(last - first + 1);
 
-	b.x2 = s2.substr(first + 2, last);
+	a.x2 = s2.substr(first + 2);
+	a.x2.erase(a.x2.end());
 
-	c.op = s2[0];
+	a.op = s2[0];
 
-	return a, b, c;
+	return a;
 }
 
 int convert_binary_to_decimal(const string& s) {
-	return 0;
+	bool negative = 0;
+
+	if (s[0] == '-') {
+		bool negative = 1;
+	}
+	
+	long binary_to_convert = stoi(s);
+
+	if (negative) {
+		binary_to_convert *= -1;
+	}
+
+	int decimal_number = 0;
+	int i = 0;
+	int remainder;
+
+	while (binary_to_convert != 0) {
+		remainder = binary_to_convert % 10;
+		binary_to_convert /= 10;
+		decimal_number += remainder * pow(2, i);
+		++i;
+	}
+
+	if (negative) {
+		decimal_number *= -1;
+	}
+	std::cout << decimal_number;
+	return decimal_number;
 }
 
 string convert_decimal_to_binary(int n) {
@@ -49,50 +75,54 @@ string convert_decimal_to_binary(int n) {
 	return s;
 }
 
-int main() 
+int main()
 {
 	char response = 'y';
 
 	while (response == 'y') {
+		calc_bin_data a;
 		string input;
 
 		std::cout << "Enter an expression <in binary format>: ";
 		std::cin >> input;
 
-		calc_parsing(input);
+		a = calc_parsing(input);
 
-		int firstNumber = stoi(a.x1);
-		int secondNumber = stoi(b.x2);
-		int resultNumber;
+		int first_number = convert_binary_to_decimal(a.x1);
+		int second_number = convert_binary_to_decimal(a.x2);
+		int result_number;
 
-		if (c.op == '+') {
-			resultNumber = firstNumber + secondNumber;
+		if (a.op == '+') {
+			result_number = first_number + second_number;
 		}
-		else if (c.op == '-') {
-			resultNumber = firstNumber - secondNumber;
+		else if (a.op == '-') {
+			result_number = first_number - second_number;
 		}
-		else if (c.op == '*') {
-			resultNumber = firstNumber * secondNumber;
+		else if (a.op == '*') {
+			result_number = first_number * second_number;
 		}
-		else if (c.op == '/') {
-			resultNumber = firstNumber / secondNumber;
+		else if (a.op == '/') {
+			result_number = first_number / second_number;
 		}
 		else {
-			resultNumber = firstNumber % secondNumber;
+			result_number = first_number % second_number;
 		}
 
+		string binary_result = convert_decimal_to_binary(result_number);
 
-		string firstBinary = convert_decimal_to_binary(firstNumber);
-		string secondBinary = convert_decimal_to_binary(secondNumber);
-		string resultBinary = convert_decimal_to_binary(resultNumber);
+		string result_sign = "";
 
-		std::cout << "--> in binary representation: " << input << "=" << resultBinary;
+		if (result_number < 0) {
+			result_sign = "-";
+		}
+		
+		std::cout << "--> in binary representation: " << input << "=" << result_sign << binary_result << endl;
+		std::cout << "--> in decimal representation: <" << first_number << ">" << a.op << "<"
+			<< second_number << ">=" << result_number << endl << endl;
 
-
-
-
-
-
+		std::cout << "Continue? <y/n> ";
+		std::cin >> response;
+		std::cout << endl;
 	}
 
 	return 0;
